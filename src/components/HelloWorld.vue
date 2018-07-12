229d1f38ac5d891af25a474921cbb6c4864d03e4
bulong <bulong.wkp@dtyunxi.com>
2018-07-12 19:47:25 +0800

    add markdown loader

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,43 +1,26 @@
 <template>
-  <div class="hello">
-   <el-row>
-    <el-button>默认按钮</el-button>
-    <el-button type="primary">主要按钮</el-button>
-    <el-button type="success">成功按钮</el-button>
-    <el-button type="info">信息按钮</el-button>
-    <el-button type="warning">警告按钮</el-button>
-    <el-button type="danger">危险按钮</el-button>
-  </el-row>
-
+  <div class="markdown-body">
+    <markdown></markdown>
   </div>
 </template>
 
 <script>
+import markdown from "../docs/markdown.md";
+import "highlight.js/styles/github.css";
+import "github-markdown-css";
+
 export default {
-  name: "HelloWorld",
-  data() {
-    return {
-      msg: "Welcome to Your Vue.js App"
-    };
+  components: {
+    markdown
   }
 };
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1,
-h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+<style>
+.markdown-body {
+  box-sizing: border-box;
+  min-width: 200px;
+  max-width: 980px;
+  margin: 0 auto;
+  padding: 45px;
 }
 </style>
